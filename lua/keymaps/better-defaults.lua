73a4ee9d4448950a504041d7c21269f6b03fbf76
Razkaroth <rocker.ikaros@gmail.com>
2023-10-29 02:34:18 -0600

    Added diagnostic remaps

--- lua/keymaps/better-defaults.lua
+++ lua/keymaps/better-defaults.lua
@@ -5,3 +5,9 @@ keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Remap for dealing with word wrap
 keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
